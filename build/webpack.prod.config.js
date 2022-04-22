8b6926e2688e957f4e25db7cdf733fc850b70720
haptear <haptear@hotmail.com>
2022-04-22 15:34:55 +0800

    Update webpack.prod.config.js

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -3,14 +3,11 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin')
 const { merge } = require('webpack-merge');
-const QiniuUploadPlugin = require('qiniu-upload-plugin');
-const qiniuConfig = require('./qiniu.config');
 const baseConfig = require('./webpack.base.config');
 
 const prodConfig = {
   mode: 'production',
   output: {
-    publicPath: qiniuConfig.publicPath,
     path: path.resolve('./dist/client'),
     filename: 'cdfang-spider-[name]-[contenthash].js'
   },
@@ -75,10 +72,5 @@ const prodConfig = {
     'react-dom': 'ReactDOM',
   }
 };
-
-// ci 环境不上传 cdn
-if (process.env.BUILD_ENV !== 'ci' && process.env.BUILD_ENV !== 'analysis') {
-  prodConfig.plugins.push(new QiniuUploadPlugin(qiniuConfig));
-}
 
 module.exports = merge(baseConfig, prodConfig);
